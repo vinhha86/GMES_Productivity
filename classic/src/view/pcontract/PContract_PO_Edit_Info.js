653616ea255a395f3c12f6f43c98509147fb0123
hungdaibang <nthung.dscvn@gmail.com>
2020-06-04 16:30:04 +0700

    PO Edit

--- classic/src/view/pcontract/PContract_PO_Edit_Info.js
+++ classic/src/view/pcontract/PContract_PO_Edit_Info.js
@@ -1,19 +1,89 @@
 Ext.define('GSmartApp.view.planporder.PContract_PO_Edit_Info', {
     extend: 'Ext.form.Panel',
     xtype: 'PContract_PO_Edit_Info',
-    layout: 'hbox',
+    layout: 'vbox',
     items: [
         {
             xtype: 'textfield',
             fieldLabel: 'PO Buyer:',
             labelAlign: 'left',
-            labelWidth: 100,
+            labelWidth: 90,
+            width: 220,
+            margin: 1
         }, 
         {
             xtype: 'textfield',
             fieldLabel: 'PO Vendor:',
             labelAlign: 'left',
-            labelWidth: 100,
+            labelWidth: 90,
+            width: 220,
+            margin: 1
         }, 
+        {
+            xtype: 'numberfield',
+            fieldLabel: 'Số lượng:',
+            labelAlign: 'left',
+            labelWidth: 90,
+            width: 220,
+            margin: 1
+        }, 
+        {
+            xtype: 'datefield',
+            fieldLabel: 'Ngày giao:',
+            labelAlign: 'left',
+            labelWidth: 90,
+            width: 220,
+            margin: 1,
+            format: 'd/m/Y',
+        }, 
+        {
+            xtype: 'datefield',
+            fieldLabel: 'Ngày NPL về:',
+            labelAlign: 'left',
+            labelWidth: 90,
+            width: 220,
+            margin: 1,
+            format: 'd/m/Y',
+        }, 
+        {
+            xtype: 'numberfield',
+            fieldLabel: 'Giá CMPT:',
+            labelAlign: 'left',
+            labelWidth: 90,
+            width: 220,
+            margin: 1
+        }, 
+        {
+            xtype: 'numberfield',
+            fieldLabel: 'Giá FOB:',
+            labelAlign: 'left',
+            labelWidth: 90,
+            width: 220,
+            margin: 1
+        }, 
+        {
+            xtype: 'numberfield',
+            fieldLabel: 'Tổng giá chào:',
+            labelAlign: 'left',
+            labelWidth: 90,
+            width: 220,
+            margin: 1
+        }, 
+        {
+            xtype: 'numberfield',
+            fieldLabel: 'Sewing Target:',
+            labelAlign: 'left',
+            labelWidth: 90,
+            width: 220,
+            margin: 1
+        },
+        {
+            xtype: 'numberfield',
+            fieldLabel: 'Sewing Fact:',
+            labelAlign: 'left',
+            labelWidth: 90,
+            width: 220,
+            margin: 1
+        },
     ]
 })
---
+++ classic/src/view/pcontract/PContract_PO_ProductList.js
@@ -0,0 +1,48 @@
+Ext.define('GSmartApp.view.pcontract.PContract_PO_ProductList', {
+    extend: 'Ext.tree.Panel',
+    xtype: 'PContract_PO_ProductList',
+    id:'PContract_PO_ProductList',
+    // controller: 'PContractSKU_ListProductViewCotroller',
+    IdPContract: 0,
+    viewConfig: {
+        enableTextSelection: true,
+        columnLines: true,
+        rowLines: true
+    },
+    selModel: {
+        //selType: 'checkboxmodel',
+        mode: 'SINGLE'
+    },
+    rootVisible: false,
+    // plugins: {
+    //     cellediting: {
+    //         clicksToEdit: 1,
+    //         listeners: {
+    //             edit: 'onEdit'
+    //         } 
+    //     }
+    // },
+    bind:{
+        store:'{PContractProductTreeStore}'
+    },
+    reference: 'PContract_DeliveryPlan_ProductList',
+    columns:[{
+        text:'Tên SP',
+        xtype: 'treecolumn',
+        dataIndex:'text',
+        flex: 1
+    },{
+        text:'Mã SP',
+        dataIndex:'code',
+        width: 60
+    },{
+        text:'Ảnh',
+        dataIndex:'imgproduct',
+        width: 50,
+        textAlign: 'center',
+        renderer: function(value, meta, record){
+            return '<img style="width:16px; height:14px" src="data:image/gif;base64,'+ value +'">';
+        }
+    }]    
+});
+
