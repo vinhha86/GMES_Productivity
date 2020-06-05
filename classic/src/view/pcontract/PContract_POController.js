dfd4587c25a7a12b2bce88c19d94b2ca6f01e6e2
PhongNgh <phongngh91@gmail.com>
2020-06-05 15:25:27 +0700

    no message

--- classic/src/view/pcontract/PContract_POController.js
+++ classic/src/view/pcontract/PContract_POController.js
@@ -1,6 +1,11 @@
 Ext.define('GSmartApp.view.pcontract.PContract_POController', {
     extend: 'Ext.app.ViewController',
     alias: 'controller.PContract_POController',
+    control: {
+        'PContract_PO_ProductList': {
+            itemclick: 'onSelectProduct'
+        }
+    },
     onFactoriesTap: function(){
         var panel_factories = this.getView().items.get('panel_factories');
         console.log(panel_factories);
@@ -12,25 +17,55 @@ Ext.define('GSmartApp.view.pcontract.PContract_POController', {
         }
     },
     onAddPOTap: function(){
-        var form = Ext.create('Ext.window.Window', {
-            closable: true,
-            resizable: false,
-            modal: true,
-            border: false,
-            title: 'Kế hoạch giao hàng',
-            closeAction: 'destroy',
-            height: 650,
-            width: 1200,
-            bodyStyle: 'background-color: transparent',
-            layout: {
-                type: 'fit', // fit screen for window
-                padding: 5
-            },
-            items: [{
-                xtype: 'PContract_PO_Edit'
-            }]
-        });
-        form.show();
+        var viewModel = this.getViewModel();
+        if(viewModel.get('productpairid_link') == 0){
+            Ext.Msg.show({
+                title: 'Bạn chưa chọn sản phẩm',
+                msg: null,
+                buttons: Ext.MessageBox.YES,
+                buttonText: {
+                    yes: 'Đóng',
+                }
+            });
+        }
+        else {
+            var form = Ext.create('Ext.window.Window', {
+                closable: true,
+                resizable: false,
+                modal: true,
+                border: false,
+                title: 'Kế hoạch giao hàng',
+                closeAction: 'destroy',
+                height: 650,
+                width: 1200,
+                bodyStyle: 'background-color: transparent',
+                layout: {
+                    type: 'fit', // fit screen for window
+                    padding: 5
+                },
+                items: [{
+                    xtype: 'PContract_PO_Edit',
+                    viewModel: {
+                        data: {
+                            productpairid_link: viewModel.get('productpairid_link'),
+                            pcontractid_link: viewModel.get('PContract.id'),
+                            plan: {
+                                id: null,
+                                pcontractid_link: viewModel.get('PContract.id'),
+                                productid_link: viewModel.get('productpairid_link')
+                            }
+                        }
+                    }
+                }]
+            });
+            form.show();
+        }
+
+        
+    },
+    onSelectProduct: function(m, rec){
+        var viewModel = this.getViewModel();
+        viewModel.set('productpairid_link', rec.data.id);
     }
 });
 
