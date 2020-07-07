da6040aff5d9a28725979ea4d2cddc7ba1b436a7
Phong Thần <napoleon.ptit@gmail.com>
2020-07-07 09:37:39 +0700

    UPDATE

--- classic/src/view/Schedule/Plan/Schedule_plan_ViewController.js
+++ classic/src/view/Schedule/Plan/Schedule_plan_ViewController.js
@@ -6,38 +6,66 @@ Ext.define('GSmartApp.view.Schedule.Plan.Schedule_plan_ViewController', {
         var crud = grid.getCrudManager();
         crud.load();
     },
-    onZoomIn : function () {
-        var panel_plan = this.getView().items.get('panel_plan');
-        panel_plan.zoomIn();
+    control: {
+        'Schedule_plan_View' : {
+            eventcontextmenu : 'onContextMenu',
+            aftereventresize : 'onResizeSchedule'
+        }
     },
+    onContextMenu : function( scheduler, eventRecord, e, eOpts) {
+        var menu_grid = new Ext.menu.Menu({
+            items: [{
+                text: 'Hợp đồng',
+                iconCls: 'x-fa fa-cart-arrow-down',
+                handler: function () {
+                    
+                }
+            }, 
+            {
+                text: 'Thông tin đơn hàng',
+                iconCls: 'x-fa fa-handshake-o',
+                handler: function () {
+                }
+            },
+            {
+                text: 'Tiến độ sản xuất',
+                iconCls: 'x-fa fa-line-chart',
+                handler: function () {
+                }
+            },
+            {
+                text: 'Năng suất',
+                iconCls: 'x-fa fa-line-chart',
+                handler: function () {
+                }
+            }
+            ]
+        })
+        e.stopEvent();
+        menu_grid.showAt(e.getXY());
+    },
+    onResizeSchedule: function(scheduler, record, eOpts) {
+        var params = new Object();
+        params.data = record.data;
 
-    onZoomOut: function () {
-        var panel_plan = this.getView().items.get('panel_plan');
-        panel_plan.zoomOut();
-    },   
-    onGrantToOrgTap: function(){
-        //var panel_orderwaiting = this.getView().up().items.get('panel_orderwaiting');
-        var panel_orderungranted = this.getView().items.get('panel_orderungranted');
-        if (null != panel_orderungranted){
-            if (panel_orderungranted.getHidden())
-                panel_orderungranted.setHidden(false);
-            else
-                panel_orderungranted.setHidden(true);
-        }
-    },   
-    onGuessView: function(){
-        //var panel_orderwaiting = this.getView().up().items.get('panel_orderwaiting');
-        var panel_guessview = this.getView().items.get('panel_guessview');
-        if (null != panel_guessview){
-            if (panel_guessview.getHidden())
-                panel_guessview.setHidden(false);
-            else
-                panel_guessview.setHidden(true);
-        }
-    },   
-    onExport: function(){
-        var me = this.getView().down('#panel_plan');
-        me.getPlugin('export').setFileFormat('pdf');
-        me.showExportDialog();
-    }    
+        GSmartApp.Ajax.post('/api/v1/schedule/update', Ext.JSON.encode(params),
+            function (success, response, options) {
+                if (success) {
+                    var response = Ext.decode(response.responseText);
+                    if (response.respcode == 200) {
+                       var data = response.data;
+                       record.set('duration', data.duration);
+                    }
+                } else {
+                    Ext.Msg.show({
+                        title: 'Cập nhật thất bại',
+                        msg: null,
+                        buttons: Ext.MessageBox.YES,
+                        buttonText: {
+                            yes: 'Đóng',
+                        }
+                    });
+                }
+            })
+    }
 })
