8149ebe58849c830255fbcf6b70b314998e34210
Ha Nguyen Vinh <vinhha2701@gmail.com>
2020-12-10 12:55:14 +0700

    update

--- classic/src/view/porders/POrder_List/POrder_List_DetailViewController.js
+++ classic/src/view/porders/POrder_List/POrder_List_DetailViewController.js
@@ -95,7 +95,7 @@ Ext.define('GSmartApp.view.porders.POrder_List.POrder_List_DetailViewController'
         var viewmodel = this.getViewModel();
 
         if (newCard.xtype == "POrder_Tab_Info") {
-            let infoView = me.down('#POrder_InfoView');
+            var infoView = me.down('#POrder_InfoView');
             infoView.IdPOrder = me.IdPOrder;
             infoView.getController().loadInfo(me.IdPOrder);
 
@@ -104,7 +104,7 @@ Ext.define('GSmartApp.view.porders.POrder_List.POrder_List_DetailViewController'
             porderSKUStore.removeAll();
         }
         else if (newCard.xtype == "POrder_Tab_Grant") {
-            let listGrantView = me.down('#POrder_List_GrantView');
+            var listGrantView = me.down('#POrder_List_GrantView');
             listGrantView.IdPOrder = me.IdPOrder;
             listGrantView.getController().loadInfo(me.IdPOrder);
         }
@@ -124,21 +124,21 @@ Ext.define('GSmartApp.view.porders.POrder_List.POrder_List_DetailViewController'
         }
     },
     onLoadData: function (id) {
-        let me = this.getView();
-        let viewmodel = this.getViewModel();
+        var me = this.getView();
+        var viewmodel = this.getViewModel();
         viewmodel.set('IdPOrder', id);
 
         me.IdPOrder = id;
 
-        let infoView = me.down('#POrder_InfoView');
+        var infoView = me.down('#POrder_InfoView');
         infoView.IdPOrder = id;
         infoView.getController().loadInfo(me.IdPOrder);
 
-        // let productSkuView = me.down('#POrder_ProductSKUView');
+        // var productSkuView = me.down('#POrder_ProductSKUView');
         // productSkuView.IdPOrder = id;
         // productSkuView.getController().loadInfo(me.IdPOrder);
 
-        // let listGrantView = me.down('#POrder_List_GrantView');
+        // var listGrantView = me.down('#POrder_List_GrantView');
         // listGrantView.IdPOrder = id;
         // listGrantView.getController().loadInfo(me.IdPOrder);
     },
@@ -147,23 +147,30 @@ Ext.define('GSmartApp.view.porders.POrder_List.POrder_List_DetailViewController'
     },
     onTTSanPham: function(){
         // pcontractid_link
-        let viewModel = this.getViewModel();
-        let pcontractid_link = viewModel.get('porder').pcontractid_link;
-        let productid_link = viewModel.get('porder').productid_link;
+        var viewModel = this.getViewModel();
+        var pcontractid_link = viewModel.get('porder').pcontractid_link;
+        var productid_link = viewModel.get('porder').productid_link;
         // console.log(pcontractid_link);
         // console.log(viewModel.get('porder'));
-        let window = Ext.create('GSmartApp.view.PContract.PContract_General_InfoView', {
+        var window = Ext.create('GSmartApp.view.PContract.PContract_General_InfoView', {
             IdPContract: pcontractid_link,
-            IdProduct: productid_link
+            IdProduct: productid_link,
+            viewModel: {
+                data: {
+                    IdPContract: eventRecord.data.pcontractid_link,
+                    IdProduct: eventRecord.data.productid_link,
+                    isWindow: true
+                }
+            }
         });
         window.show();
     },
     onTTDonHang: function(){
         // pcontract_poid_link
-        let viewModel = this.getViewModel();
-        let pcontract_poid_link = viewModel.get('porder').pcontract_poid_link;
+        var viewModel = this.getViewModel();
+        var pcontract_poid_link = viewModel.get('porder').pcontract_poid_link;
         // console.log(pcontract_poid_link);
-        let window = Ext.create('GSmartApp.view.pcontract.PContract_PO_Edit_Info_Main_Window', {
+        var window = Ext.create('GSmartApp.view.pcontract.PContract_PO_Edit_Info_Main_Window', {
             viewModel: {
                 data: {
                     id: pcontract_poid_link
