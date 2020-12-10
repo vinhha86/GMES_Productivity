ce106b169d948412de4cdcc4cb9d682de7334db0
Phong <phongngh91@gmail.com>
2020-12-10 16:57:42 +0700

    Update PContract_PO_Edit_Info_Main_Controller.js

--- classic/src/view/pcontract/PContract_PO/Info/PContract_PO_Edit_Info_Main_Controller.js
+++ classic/src/view/pcontract/PContract_PO/Info/PContract_PO_Edit_Info_Main_Controller.js
@@ -110,8 +110,7 @@ Ext.define('GSmartApp.view.pcontract.PContract_PO_Edit_Info_Main_Controller', {
     },
     onThoat: function(){
         var viewmodel = this.getViewModel();
-        this.fireEvent('Thoat');
-        this.fireEvent("ThoatAndUpdate",  viewmodel.get('pcontract_po_productivity'));
+        this.fireEvent('Thoat',viewmodel.get('pcontract_po_productivity'));
     },
     onSave: function(){
         var me = this;
