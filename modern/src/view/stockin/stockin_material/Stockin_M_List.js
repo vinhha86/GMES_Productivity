9c711c8dbe92528359ecd07998e93fab437415f3
Ha Nguyen Vinh <vinhha2701@gmail.com>
2021-03-10 17:07:10 +0700

    update

--- modern/src/view/stockin/stockin_material/Stockin_M_List.js
+++ modern/src/view/stockin/stockin_material/Stockin_M_List.js
@@ -29,16 +29,35 @@ Ext.define('GSmartApp.view.stockin.Stockin_M_List', {
     itemTpl:
             '<div class="content">' +
                 '<div class="content1">' +
-                '<div class="content1-sub1"><b>Số phiếu:</b> <b>{stockincode}</b></div>' +
-                // '<div class="content1-sub2"><b>Ngày nhập:</b> {stockindate}</div>' +
-                '<div class="content1-sub2"><b>Ngày nhập:</b> {stockindate:date("m/d/Y")}</div>' +
+                    '<div class="content1-sub1"><b>Số phiếu:</b></div>'+
+                    '<div class="content1-sub1"><b>{stockincode}</b></div>' +
+                    '<div class="content1-sub1"><b>Ngày nhập:</b></div>'+
+                    '<div class="content1-sub1">{stockindate:date("m/d/Y")}</div>' +
                 '</div>' +
+
                 '<div class="content1">' +
-                '<div class="content1-sub1"><b>Số Invoice:</b> {invoice_number}</div>' +
-                '<div class="content1-sub2"><b>Ngày Invoice:</b> {invoice_date:date("m/d/Y")}</div>' +
+                    '<div class="content1-sub1"><b>Số Invoice:</b></div>'+
+                    '<div class="content1-sub1">{invoice_number}</div>' +
+                    '<div class="content1-sub1"><b>Ngày Invoice:</b></div>'+
+                    '<div class="content1-sub1">{invoice_date:date("m/d/Y")}</div>' +
                 '</div>' +
-                '<div class="content2"><b>Loại nhập:</b> {stockintype_name}</div>' +
-                '<div class="content2"><b>Nơi xuất:</b> {orgfrom_name}</div>' +
+
+                // '<div class="content1">' +
+                // '<div class="content1-sub1"><b>Số Invoice:</b> {invoice_number}</div>' +
+                // '<div class="content1-sub2"><b>Ngày Invoice:</b> {invoice_date:date("m/d/Y")}</div>' +
+                // '</div>' +
+
+                '<div class="content2">'+
+                    '<div class="content2-sub1"><b>Loại nhập:</b></div>'+
+                    '<div class="content2-sub2">{stockintype_name}</div>' +
+                '</div>' +
+
+                '<div class="content2">'+
+                    '<div class="content2-sub1"><b>Nơi xuất:</b></div>'+
+                    '<div class="content2-sub2">{orgfrom_name}</div>' +
+                '</div>' +
+
+                // '<div class="content2"><b>Nơi xuất:</b> {orgfrom_name}</div>' +
             '</div>',
 
     bind: {
