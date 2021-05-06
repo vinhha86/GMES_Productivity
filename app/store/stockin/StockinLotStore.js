84891990f79cd82f988c9a4178c732e263f1530a
Ha Nguyen Vinh <vinhha2701@gmail.com>
2021-05-06 15:01:07 +0700

    update

--- app/store/stockin/StockinLotStore.js
+++ app/store/stockin/StockinLotStore.js
@@ -38,6 +38,34 @@ Ext.define('GSmartApp.store.stockin.StockinLotStore', {
 				}
 			}
 		});
+	},
+	loadStore_byStockinId_async: function(stockinid_link){
+		var me=this;
+		var params = new Object();
+		params.stockinid_link = stockinid_link;
+		
+		this.setProxy({
+			type: 'ajax',
+			actionMethods: {
+				create : 'POST',
+				read   : 'POST',
+				update : 'POST',
+				destroy: 'POST'
+			},
+			url: config.getAppBaseUrl_Jitin()+'/api/v1/stockin/getStockinLotByStockinId',
+			paramsAsJson:true,
+			extraParams : params,
+			noCache: false,
+			headers :{
+				'Accept': "application/json", 
+				'Content-Type':"application/json"
+			 },
+			reader: {
+				type: 'json',
+				rootProperty: 'data',
+				totalProperty: 'totalCount'
+			}
+		});
 	}
    
 });
