086da29da1da9453547397efd05ffbb57bde60e6
Phong <phongngh91@gmail.com>
2021-05-12 23:07:13 +0700

    Update POrder_List_DetailView.js

--- classic/src/view/porders/POrder_List/POrder_List_DetailView.js
+++ classic/src/view/porders/POrder_List/POrder_List_DetailView.js
@@ -10,32 +10,34 @@ Ext.define('GSmartApp.view.porders.POrder_List.POrder_List_DetailView', {
     layout: 'border',
     // height: 500,
     items: [
-    {
-        region: 'center',
-        border: false,
-        margin: 1,
-        xtype: 'tabpanel',
-        itemId:'tabmain',
-        items: [{
-            title: 'Thông tin lệnh',
-            xtype: 'POrder_Tab_Info'
-        }, {
-            title: 'Phân chuyền',
-            xtype: 'POrder_Tab_Grant'
-        },{
-            title: 'Định mức',
-            xtype: 'PorderBom_TabColor'
-        },{
-            title: 'Quy trình công nghệ',
-            xtype: 'PorderSewingCost_View'
-        },{
-            title: 'Tiến độ sản xuất',
-            xtype: 'PorderProcessingDetail'
-        },{
-            title: 'Yêu cầu xuất NPL',
-            xtype: 'Stockout_order_MainView'
-        }]
-    }],
+        {
+            region: 'center',
+            border: false,
+            margin: 1,
+            xtype: 'tabpanel',
+            itemId: 'tabmain',
+            items: [{
+                title: 'Thông tin lệnh',
+                xtype: 'POrder_Tab_Info'
+            }, {
+                title: 'Phân chuyền',
+                xtype: 'POrder_Tab_Grant'
+            }, {
+                title: 'Định mức',
+                xtype: 'PorderBom_TabColor'
+            }, {
+                title: 'Định mức New'
+            }, {
+                title: 'Quy trình công nghệ',
+                xtype: 'PorderSewingCost_View'
+            }, {
+                title: 'Tiến độ sản xuất',
+                xtype: 'PorderProcessingDetail'
+            }, {
+                title: 'Yêu cầu xuất NPL',
+                xtype: 'Stockout_order_MainView'
+            }]
+        }],
     dockedItems: [{
         layout: 'hbox',
         reference: 'dockBottomBar',
@@ -56,13 +58,13 @@ Ext.define('GSmartApp.view.porders.POrder_List.POrder_List_DetailView', {
             margin: 1,
             itemId: 'btnPrintPorder',
             iconCls: 'x-fa fa-print'
-        },{
+        }, {
             xtype: 'button',
             text: 'Kế hoạch cắt',
             margin: 1,
             itemId: 'btnCutPlan',
             iconCls: 'x-fa fa-cut'
-        },{
+        }, {
             xtype: 'button',
             text: 'Thông tin sản phẩm',
             margin: 1,
@@ -74,14 +76,14 @@ Ext.define('GSmartApp.view.porders.POrder_List.POrder_List_DetailView', {
             margin: 1,
             itemId: 'btnTTDonHang',
             iconCls: 'x-fa fa-search'
-        // },
-        // {
-        //     xtype: 'button',
-        //     text: 'Lưu',
-        //     margin: 1,
-        //     itemId: 'btnLuu',
-        //     iconCls: 'x-fa fa-save',
-        //     formBind: true
+            // },
+            // {
+            //     xtype: 'button',
+            //     text: 'Lưu',
+            //     margin: 1,
+            //     itemId: 'btnLuu',
+            //     iconCls: 'x-fa fa-save',
+            //     formBind: true
         }]
     }]
 })
