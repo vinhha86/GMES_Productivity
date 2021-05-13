88efe8eb978def58ca12bf2348cb49f671469eef
Phong <phongngh91@gmail.com>
2021-05-13 10:02:03 +0700

    Update Runtime.js

--- app/config/Runtime.js
+++ app/config/Runtime.js
@@ -2,18 +2,20 @@
 /* global l10n */
 /* global config */
 
-Ext.define('GSmartApp.config.Runtime',{
+Ext.define('GSmartApp.config.Runtime', {
     alternateClassName: [
         'config'
     ],
-    singleton : true,
-    config : {
+    singleton: true,
+    config: {
         basePath: {
             dictionary: 'resources/dictionary.json'
         },
-        months: ['January','February','March','April','May','June','July','August','September','October','November','December'],
+        months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
         appBaseUrl: 'http://localhost:8990/gsmartcore',
         appBaseUrl_Jitin: 'http://localhost:8991/jitin',
+        // appBaseUrl: 'http://172.20.10.2:8990/gsmartcore',
+        // appBaseUrl_Jitin: 'http://172.20.10.2:8991/jitin',
         // appBaseUrl: 'http://gpay.vn:8090/gmes',
         // appBaseUrl: 'http://gpay.vn:8090/gmesdha',
         // appBaseUrl: 'http://27.71.231.154:8080/gmesdha',
@@ -28,14 +30,14 @@ Ext.define('GSmartApp.config.Runtime',{
         avatar: null,
         pageSize: 25,
         // MQTT config
-        deviceid:'devsim-0002',
-        termid:'term-0004',
-        clientid:'thieutv#devops01',
-        host:'gpay.vn',
-        port:8083,
+        deviceid: 'devsim-0002',
+        termid: 'term-0004',
+        clientid: 'thieutv#devops01',
+        host: 'gpay.vn',
+        port: 8083,
         porderTaskRunner: null
     },
-    constructor : function(config){
+    constructor: function (config) {
         this.initConfig(config);
         return this;
     }
