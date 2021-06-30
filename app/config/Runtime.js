26e9132be30d505056ed4d6e342b9ccd7e58f05e
Phong <phongngh91@gmail.com>
2021-06-30 15:52:50 +0700

    Update Runtime.js

--- app/config/Runtime.js
+++ app/config/Runtime.js
@@ -14,7 +14,7 @@ Ext.define('GSmartApp.config.Runtime', {
         months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
         appBaseUrl: 'http://localhost:8990/gsmartcore',
         appBaseUrl_Jitin: 'http://localhost:8991/jitin',
-        appBaseUrl_demo: 'http://localhost:9091/',
+        // appBaseUrl_demo: 'http://localhost:9091/',
 
         // appBaseUrl: 'http://172.20.10.2:8990/gsmartcore',
         // appBaseUrl_Jitin: 'http://172.20.10.2:8991/jitin',
@@ -26,6 +26,7 @@ Ext.define('GSmartApp.config.Runtime', {
         // appBaseUrl: 'http://gpay.vn:8090/gmes',
         // appBaseUrl_Jitin: 'http://gpay.vn:8090/jitin',
         back: 'http://gpay.vn:8181/oauth/',
+        appBaseUrl_demo: 'gpay.vn:9091/',
 
         //local
         qrcode_personel_url: 'http://localhost:8990/gsmartcore/api/v1/qrocde/getqr_code_personel?text=',
