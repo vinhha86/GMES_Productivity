0c1f6811f22bbc585bd2d349ca5dcac354132138
Ha Nguyen Vinh <vinhha2701@gmail.com>
2021-11-29 15:03:11 +0700

    update tong hop bao an mobile

--- modern/src/view/TongHopBaoAn/TongHopBaoAnViewController.js
+++ modern/src/view/TongHopBaoAn/TongHopBaoAnViewController.js
@@ -98,6 +98,8 @@ Ext.define('GSmartApp.view.TongHopBaoAn.TongHopBaoAnViewController', {
         params.orgid_link = orgid_link;
         params.date = date;
 
+        // return;
+
         GSmartApp.Ajax.post('/api/v1/timesheetlunch/getForTimeSheetLunch_Mobile', Ext.JSON.encode(params),
             function (success, response, options) {
                 me.setMasked(false);
@@ -112,6 +114,41 @@ Ext.define('GSmartApp.view.TongHopBaoAn.TongHopBaoAnViewController', {
                 }
             })
     },
+    // loadTongHopBaoAnView_Info: function(){
+    //     var m = this;
+    //     var me = this.getView();
+    //     var viewModel = this.getViewModel();
+    //     var TimeSheetLunchStore = viewModel.getStore('TimeSheetLunchStore');
+
+    //     var orgid_link = viewModel.get('orgId');
+    //     var date = me.down('#date').getValue();
+    //     // viewModel.set('date', date);
+
+    //     // console.log(orgid_link);
+    //     // console.log(date);
+
+    //     me.setMasked(true);
+
+    //     var params = new Object();
+    //     params.orgid_link = orgid_link;
+    //     params.date = date;
+
+    //     // return;
+
+    //     GSmartApp.Ajax.post('/api/v1/timesheetlunch/getForTimeSheetLunch_Mobile', Ext.JSON.encode(params),
+    //         function (success, response, options) {
+    //             me.setMasked(false);
+    //             if (success) {
+    //                 var response = Ext.decode(response.responseText);
+    //                 // console.log(response);
+    //                 TimeSheetLunchStore.removeAll();
+    //                 TimeSheetLunchStore.insert(0,response.data)
+    //             } else {
+    //                 // Ext.toast('Lấy thông tin thất bại', 3000);
+    //                 TimeSheetLunchStore.fireEvent('logout');
+    //             }
+    //         })
+    // },
     renderSum: function(value ){
         if (null == value) value = 0;
         return Ext.util.Format.number(value, '0,000');    
--- modern/src/view/TongHopBaoAn/TongHopBaoAnView_Info.js
+++ modern/src/view/TongHopBaoAn/TongHopBaoAnView_Info.js
@@ -1,3 +1,92 @@
+// Ext.define('GSmartApp.view.TongHopBaoAn.TongHopBaoAnView_Info', {
+//     extend: 'Ext.grid.Grid',
+//     xtype: 'TongHopBaoAnView_Info',
+//     itemId: 'TongHopBaoAnView_Info',
+//     // id:'TimeSheetLunch_Info',
+//     cls: 'TongHopBaoAnView_Info',
+//     reference: 'TongHopBaoAnView_Info',
+//     requires: [
+//         'Ext.grid.plugin.CellEditing'
+//     ],
+//     // height: '100%',
+//     // width: '100%',
+//     markDirty: true,
+//     columnLines: true,
+//     striped: false,
+//     variableHeights: true,
+//     width: '100%',
+
+//     plugins: {
+//         gridcellediting: {
+//             selectOnEdit: true
+//         },
+//     },
+
+//     selectable: {
+//         rows: true,
+//         cells: false
+//     },
+
+//     bind: {
+//         store: '{TimeSheetLunchStore}'
+//     },
+//     plugins: {
+//         gridsummaryrow: true,
+//     },
+//     columns: [
+//         {
+//             text: 'Tổ', 
+//             flex: 1,
+//             // width: 90,
+//             dataIndex: 'orgCode',
+//             align: 'center',
+//             // renderer: function(value, record, dataIndex, cell, column) {
+//             //     return value.toUpperCase();
+//             // },
+//         },
+//         {
+//             text: 'Ca 1', 
+//             flex: 1,
+//             dataIndex: 'sumCa1',
+//             align: 'center',
+//             summary: 'sum',
+//             summaryRenderer: 'renderSum'
+//         },
+//         {
+//             text: 'Ca 2', 
+//             flex: 1,
+//             dataIndex: 'sumCa2',
+//             align: 'center',
+//             summary: 'sum',
+//             summaryRenderer: 'renderSum'
+//         },
+//         {
+//             text: 'Ca 3', 
+//             flex: 1,
+//             dataIndex: 'sumCa3',
+//             align: 'center',
+//             summary: 'sum',
+//             summaryRenderer: 'renderSum'
+//         },
+//         {
+//             text: 'Ca 4', 
+//             flex: 1,
+//             dataIndex: 'sumCa4',
+//             align: 'center',
+//             summary: 'sum',
+//             summaryRenderer: 'renderSum'
+//         },
+//         {
+//             text: 'Ca 5', 
+//             flex: 1,
+//             dataIndex: 'sumCa5',
+//             align: 'center',
+//             summary: 'sum',
+//             summaryRenderer: 'renderSum'
+//         },
+//     ],
+// });
+
 Ext.define('GSmartApp.view.TongHopBaoAn.TongHopBaoAnView_Info', {
     extend: 'Ext.grid.Grid',
     xtype: 'TongHopBaoAnView_Info',
@@ -35,51 +124,36 @@ Ext.define('GSmartApp.view.TongHopBaoAn.TongHopBaoAnView_Info', {
     },
     columns: [
         {
-            text: 'Tổ', 
+            text: 'Ca', 
             flex: 1,
             // width: 90,
-            dataIndex: 'orgCode',
+            dataIndex: 'caName',
             align: 'center',
             // renderer: function(value, record, dataIndex, cell, column) {
             //     return value.toUpperCase();
             // },
         },
         {
-            text: 'Ca 1', 
+            text: 'Đăng ký', 
             flex: 1,
-            dataIndex: 'sumCa1',
+            dataIndex: 'soDangKy',
             align: 'center',
             summary: 'sum',
             summaryRenderer: 'renderSum'
         },
         {
-            text: 'Ca 2', 
+            text: 'Thêm', 
             flex: 1,
-            dataIndex: 'sumCa2',
+            dataIndex: 'soThem',
             align: 'center',
             summary: 'sum',
             summaryRenderer: 'renderSum'
         },
+        
         {
-            text: 'Ca 3', 
+            text: 'Tổng', 
             flex: 1,
-            dataIndex: 'sumCa3',
-            align: 'center',
-            summary: 'sum',
-            summaryRenderer: 'renderSum'
-        },
-        {
-            text: 'Ca 4', 
-            flex: 1,
-            dataIndex: 'sumCa4',
-            align: 'center',
-            summary: 'sum',
-            summaryRenderer: 'renderSum'
-        },
-        {
-            text: 'Ca 5', 
-            flex: 1,
-            dataIndex: 'sumCa5',
+            dataIndex: 'soTong',
             align: 'center',
             summary: 'sum',
             summaryRenderer: 'renderSum'
