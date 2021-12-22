681b227b0acbc205ad27e4b79e5c65dbb09b5561
Phong <phongngh91@gmail.com>
2021-12-22 19:27:27 +0700

    Update ChiTietBaoAnView.js

--- classic/src/view/personel/BaoCaoBaoAn/ChiTietBaoAnView.js
+++ classic/src/view/personel/BaoCaoBaoAn/ChiTietBaoAnView.js
@@ -14,6 +14,7 @@ Ext.define('GSmartApp.view.personel.BaoCaoBaoAn.ChiTietBaoAnView', {
     viewConfig: {
         enableTextSelection: true,
         stripeRows: false,
+        columnLines: true,
         getRowClass: function (record, index) {
             if (record.get('orgtypeid_link') == 166) {
                 return 'po_offer';
@@ -29,31 +30,46 @@ Ext.define('GSmartApp.view.personel.BaoCaoBaoAn.ChiTietBaoAnView', {
         text: 'Ca 1',
         width: 70,
         summaryType: 'sum',
-        summaryRenderer: 'renderSum'
+        summaryRenderer: 'renderSum',
+        renderer: function (value, metaData, record, rowIdx, colIdx, store) {
+            return (value == null || value == 0) ? "" : value;
+        }
     }, {
         dataIndex: 'ca2',
         text: 'Ca 2',
         width: 70,
         summaryType: 'sum',
-        summaryRenderer: 'renderSum'
+        summaryRenderer: 'renderSum',
+        renderer: function (value, metaData, record, rowIdx, colIdx, store) {
+            return (value == null || value == 0) ? "" : value;
+        }
     }, {
         dataIndex: 'ca3',
         text: 'Ca 3',
         width: 70,
         summaryType: 'sum',
-        summaryRenderer: 'renderSum'
+        summaryRenderer: 'renderSum',
+        renderer: function (value, metaData, record, rowIdx, colIdx, store) {
+            return (value == null || value == 0) ? "" : value;
+        }
     }, {
         dataIndex: 'ca4',
         text: 'Ca 4',
         width: 70,
         summaryType: 'sum',
-        summaryRenderer: 'renderSum'
+        summaryRenderer: 'renderSum',
+        renderer: function (value, metaData, record, rowIdx, colIdx, store) {
+            return (value == null || value == 0) ? "" : value;
+        }
     }, {
         dataIndex: 'ca5',
         text: 'Ca 5',
         width: 70,
         summaryType: 'sum',
-        summaryRenderer: 'renderSum'
+        summaryRenderer: 'renderSum',
+        renderer: function (value, metaData, record, rowIdx, colIdx, store) {
+            return (value == null || value == 0) ? "" : value;
+        }
     }]
 });
 
