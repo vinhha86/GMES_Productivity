335d669606315668cf2201bf793b67631389dad1
Phong <phongngh91@gmail.com>
2022-01-13 23:09:03 +0700

    Update TimeSheetInOutStore.js

--- app/store/TimeSheetInOut/TimeSheetInOutStore.js
+++ app/store/TimeSheetInOut/TimeSheetInOutStore.js
@@ -1,30 +1,27 @@
 Ext.define('GSmartApp.store.TimeSheetInOut.TimeSheetInOutStore', {
 	extend: 'Ext.data.Store',
 	alias: 'store.TimeSheetInOutStore',
-    fields: [
-        {name: 'id'},
-        {name: 'name', type: 'string'},
-        {name: 'code', type: 'string'},
-        {name: 'timerecorded'},
+	fields: [
+		{ name: 'id' },
+		{ name: 'name', type: 'string' },
+		{ name: 'code', type: 'string' },
+		{ name: 'timerecorded' },
 		{
-            name    : 'hour', 
-            convert : function (value, rec) {
-                var result ='';
-                var  timerecorded= new Date(rec.get('timerecorded'));
-                var timerecorded_hour = timerecorded.getHours() < 10 ? '0'+ timerecorded.getHours(): timerecorded.getHours();
-                var timerecorded_minute = timerecorded.getMinutes() < 10 ? '0'+ timerecorded.getMinutes() : timerecorded.getMinutes();
-                result = timerecorded_hour + ':' + timerecorded_minute;
-                return  result;
-            }
-        },
-
+			name: 'hour',
+			convert: function (value, rec) {
+				var result = '';
+				var timerecorded = new Date(rec.get('timerecorded'));
+				var timerecorded_hour = timerecorded.getHours() < 10 ? '0' + timerecorded.getHours() : timerecorded.getHours();
+				var timerecorded_minute = timerecorded.getMinutes() < 10 ? '0' + timerecorded.getMinutes() : timerecorded.getMinutes();
+				result = timerecorded_hour + ':' + timerecorded_minute;
+				return result;
+			}
+		}
 	],
-
-	loadStore: function (todate,fromdate) {
-
+	loadStore: function (todate, fromdate) {
 		var params = new Object();
-        params.todate = todate;
-        params.fromdate = fromdate;
+		params.todate = todate;
+		params.fromdate = fromdate;
 		this.setProxy({
 			type: 'ajax',
 			actionMethods: {
