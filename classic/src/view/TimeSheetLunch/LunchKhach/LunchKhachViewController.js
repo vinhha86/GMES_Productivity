8267e2a6f79166e9fbce862006455ee87161dca3
Ha Nguyen Vinh <vinhha2701@gmail.com>
2022-03-16 19:43:37 +0700

    update

--- classic/src/view/TimeSheetLunch/LunchKhach/LunchKhachViewController.js
+++ classic/src/view/TimeSheetLunch/LunchKhach/LunchKhachViewController.js
@@ -2,7 +2,12 @@ Ext.define('GSmartApp.view.TimeSheetLunch.LunchKhachViewController', {
     extend: 'Ext.app.ViewController',
     alias: 'controller.LunchKhachViewController',
     init: function () {
-
+        var viewModel = this.getViewModel();
+        var TimeSheetLunchKhachStore = viewModel.getStore('TimeSheetLunchKhachStore');
+        TimeSheetLunchKhachStore.getSorters().add({
+            property: 'shifttypeid_link',
+            direction: 'ASC'
+        });
     },
     onEdit: function (editor, context, e) {
         var viewmodel = this.getViewModel();
